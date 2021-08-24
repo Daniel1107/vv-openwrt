0406a8dc9d17f041a279dd6e74e1a33a270b1d21
Daniel1107 <50655850+Daniel1107@users.noreply.github.com>
2021-08-24 10:33:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,6 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default      
-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default  
-git clone https://github.com/destan19/OpenAppFilter' >>feeds.conf.default   
-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default  
-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default 
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
